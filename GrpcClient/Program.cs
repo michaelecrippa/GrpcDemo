914509ce30dd4e2e5e87b8960988377653e08c33
Michael Crippa <michaelecrippa@gmail.com>
2022-09-20 19:32:24 +0300

    Add GrpcDemo solution, including GrpcClient and GrpcServer with sample services

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using GrpcClient.Protos;
+
+
+var channel = GrpcChannel.ForAddress("http://localhost:5045");
+
+//var client = new Greeter.GreeterClient(channel);
+//var request = new HelloRequest() { Name = "Mike" };
+ 
+var customerClient = new Customer.CustomerClient(channel);
+
+var lookupModel = new CustomerLookupModel() {  UserId = 1 };
+var customer = await customerClient.GetCustomerInfoAsync(lookupModel);
+
+Console.WriteLine(customer.FirstName);
+Console.WriteLine(customer.LastName);
+
+using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+{
+    Console.WriteLine("New Customers:");
+    while (await call.ResponseStream.MoveNext(default))
+    {
+        var newCustomer = call.ResponseStream.Current;
+
+        Console.WriteLine(newCustomer.FirstName);
+    }
+}
+
+Console.ReadLine();
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,58 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            var output = new CustomerModel() 
+            { 
+                FirstName = "Michael", 
+                LastName = "Crippa", 
+                EmailAdress = "michaelecrippa@gmail.com", 
+                Age = 21, 
+                IsAlive = true 
+            };
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            var customers = new List<CustomerModel>()
+            {
+                new CustomerModel()
+                {
+                    FirstName = "Nike",
+                    LastName = "Arrie",
+                    EmailAdress = "nikeArrie@gmail.com",
+                    Age = 26,
+                    IsAlive = false
+                },
+                new CustomerModel()
+                {
+                    FirstName = "Michael",
+                    LastName = "Crippa",
+                    EmailAdress = "michaelecrippa@gmail.com",
+                    Age = 21,
+                    IsAlive = true
+                }
+            };
+
+            foreach (var customer in customers)
+            {
+                await Task.Delay(690);
+                await responseStream.WriteAsync(customer);
+            }
+        }
+    }
+}
